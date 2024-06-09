af9081f9a537ea97f22d5cd06ddba43b6658d7b5
GuerraSalasIvan <guerrasalasivan@gmail.com>
2024-06-09 17:43:49 +0200

    prueba 2

--- config/sanctum.php
+++ config/sanctum.php
@@ -4,7 +4,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        '34.202.233.105,localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
         env('APP_URL') ? ','.parse_url(env('APP_URL'), PHP_URL_HOST) : '',
         env('FRONTEND_URL') ? ','.parse_url(env('FRONTEND_URL'), PHP_URL_HOST) : ''
     ))),
